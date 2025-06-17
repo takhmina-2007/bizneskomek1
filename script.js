fd046d20aa5beb91f3e6513fdf5e098cb53ed768
takhmina-2007 <takhmina.marlen@narxoz.kz>
2025-06-17 08:02:32 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="kk">
+<head>
+  <meta charset="UTF-8">
+  <title data-i18n="home_title">Басты бет</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+  <button onclick="switchLanguage()">KZ / RU</button>
+
+  <h1 data-i18n="main_header">ШОБ-ты нөлден бастау</h1>
+  <p data-i18n="main_subheader">Нұсқаулық + Telegram ботпен бизнесіңді баста</p>
+
+  <div class="buttons">
+    <a href="instruct.html" class="btn" data-i18n="btn_instruction">Инструктажды оқу</a>
+    <a href="telegram.html" class="btn" data-i18n="btn_bot">Telegram-ботқа өту</a>
+    <a href="contact.html" class="btn" data-i18n="btn_contact">📞 Байланыс</a>
+  </div>
+
+  <script src="script.js"></script>
+</body>
+</html>
